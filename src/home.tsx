c6cfd0d8ee725143b7af6b7b00463c265084b148
fxcbsby <fxcbsby@163.com>
2022-11-23 14:33:21 +0800

    更新

--- src/home.tsx
+++ src/home.tsx
@@ -15,12 +15,14 @@ interface HomeStates{
     count:number
 }
 
+
 export default function Home(){
     const count = useSelector((state)=>state.home.count)
     console.log('更新111')
     const dispatch = useDispatch()
     useEffect(()=>{
         console.log('更新')
+        // console.log(this.nodeEle)
     })
     const  incrementFn= ()=>{
         dispatch(increment(2))
@@ -29,11 +31,13 @@ export default function Home(){
         dispatch(decrement(2))
         
     }
+    // const clickMe = ()=>{
+
+    // }
 
     return (
         <>
             <p>111: {count}</p>
-
             <button onClick={e=>incrementFn()}>加2</button>
             <button onClick={e=>decrementFn()}>减2</button>
         </>
--- webpack.base.js
+++ webpack.base.js
@@ -3,6 +3,7 @@ let webpack = require('webpack')
 let { CleanWebpackPlugin } = require('clean-webpack-plugin')
 let HtmlWebpackPlugin = require("html-webpack-plugin")
 let cssExtract = require('mini-css-extract-plugin')
+let HappyPack = require('happypack')
 
 module.exports = {
     entry:{
@@ -19,40 +20,42 @@ module.exports = {
             {
                 test:/\.jsx?$/,
                 use:[
-                    'babel-loader',
+                    'babel-loader?cacheDirectory=true',
                     'eslint-loader'
                 ]
             },
             {
                 test:/\.tsx?$/,
                 include:path.resolve(__dirname,'src'),
-                use:[
-                    {
-                        loader: require.resolve('babel-loader'),
+                use: 'happypack/loader?id=tsx'
+                // use:[
+                //     {
+                //         loader: 'babel-loader?cacheDirectory=true',
                         
-                        options: {
-                            customize: require.resolve(
-                                'babel-preset-react-app/webpack-overrides'
-                              ),
-                              presets: [
-                                [
-                                  require.resolve('babel-preset-react-app'),
+                //         options: {
+                //             customize: require.resolve(
+                //                 'babel-preset-react-app/webpack-overrides'
+                //               ),
+                //               presets: [
+                //                 [
+                //                   require.resolve('babel-preset-react-app'),
                                 
-                                ],
-                              ],
-                        },
-                    },
-                    // require.resolve('ts-loader'),
-                    {
-                        loader:'eslint-loader',
-                        options:{
-                            quiet: true
-                        }
-                    },
-                ]
+                //                 ],
+                //               ],
+                //         },
+                //     },
+                //     // require.resolve('ts-loader'),
+                //     {
+                //         loader:'eslint-loader',
+                //         options:{
+                //             quiet: true
+                //         }
+                //     }
+                // ]
             },
             {
                 test: /(\.css)$/,
+                // use: 'happypack/loader?id=scss'
                 use:[
                     cssExtract.loader,
                     'css-loader',
@@ -61,6 +64,7 @@ module.exports = {
             },
             {
                 test: /(\.s(c|a)ss)$/,
+                // use: 'happypack/loader?id=scss'
                 use:[
                     cssExtract.loader,
                     'css-loader',
@@ -78,6 +82,33 @@ module.exports = {
         new cssExtract({
             filename:'css/index[contenthash:3].css',
             chunkFilename:'css/[name].chunk[chunkhash:3].css'
+        }),
+        new HappyPack({
+            id: 'tsx',
+            threads: 2,
+            loaders:[
+                {
+                    loader: 'babel-loader',
+                    
+                    options: {
+                        customize: require.resolve(
+                            'babel-preset-react-app/webpack-overrides'
+                          ),
+                          presets: [
+                            [
+                              require.resolve('babel-preset-react-app'),
+                            
+                            ],
+                          ],
+                    },
+                },
+                {
+                    loader:'eslint-loader',
+                    options:{
+                        quiet: true
+                    }
+                }
+            ]
         }),
         //定义全局变量
         new webpack.DefinePlugin({
